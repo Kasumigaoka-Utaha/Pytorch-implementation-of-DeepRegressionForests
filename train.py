0080407d46401612ed60e667df470eac7a4afd89
Kasumigaoka-Utaha <45712215+Kasumigaoka-Utaha@users.noreply.github.com>
2021-05-11 10:06:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -33,10 +33,11 @@ def main():
   parser.add_argument('--tree', type=int, required=False, default=5)
   parser.add_argument('--depth', type=int, required=False, default=6)
   parser.add_argument("--data_dir", type=str, required=True, help="Data root directory")
-  parser.add_argument("--resume", type=str, default=None, help="Resume from checkpoint if any")
-  parser.add_argument("--checkpoint", type=str, default="checkpoint", help="Checkpoint directory")
   parser.add_argument('--nout', type=int, required=False, default=128)
-  parser.add_argument('--save', type=str, required=False, default='model')
+  # functions will be updated later
+  # parser.add_argument("--resume", type=str, default=None, help="Resume from checkpoint if any")
+  # parser.add_argument("--checkpoint", type=str, default="checkpoint", help="Checkpoint directory")
+  # parser.add_argument('--save', type=str, required=False, default='model')
   args=parser.parse_args()
 
   #testdir = './data/morph/'  #directory for test dataset
@@ -45,8 +46,7 @@ def main():
 
   # some useful options ##
   ntree = args.tree      #
-  treeDepth = args.depth # 
-  #test_batch_size = 15   #
+  treeDepth = args.depth #   
   ########################
 
   #system initialize
@@ -67,7 +67,6 @@ def main():
   test_loader = DataLoader(test_dataset, batch_size=cfg.TEST.BATCH_SIZE, shuffle=False,
                             num_workers=cfg.TRAIN.WORKERS, drop_last=False)
   print('Testing data finished...')
-  #initialize the network
   if args.nout > 0:
     assert(args.nout >= int(pow(2, treeDepth - 1) - 1))
     nout = args.nout
@@ -76,27 +75,28 @@ def main():
       nout = int(pow(2, treeDepth - 1) - 1)
     else:
       nout = int((pow(2, treeDepth - 1) - 1) * ntree * 2 / 3)
-
+  #initialize the network
   print('Build the DRF Network...')
   start_epoch = 0
   Solver = solver(device,nout=nout,num_tree=ntree,depth=treeDepth)
   print('Successfully build the DRF Network...')
-  
+  # training process, which can refer to solver.py
+  best_kl = 0.0
+  best_mae = 1000
   for epoch in range(start_epoch,start_epoch+200):
     #train
     train_loss,train_acc = Solver.train(train_loader,epoch,device)
     print('In current epoch, train_loss_avg is ',train_loss,' train_acc_avg is ',train_acc)
     #test
-    test_loss,test_acc,mae = Solver.test(test_loader,epoch,device)
-    print('In current epoch, test_loss_avg is ',test_loss,' test_acc_avg is ',test_acc,' test_mae is ',mae)
-    #checkpoint
-    if best_acc < test_acc:
-      best_acc = test_acc
-    if best_mae < mae:
-      best_mae = mae
+    res_kl,res_mae = Solver.test(test_loader,epoch,device)
+    print('In current epoch, mae: %.4f  cs: %.4f'%(res_mae, res_kl))
+    if best_kl < res_kl:
+      best_kl = res_kl
+    if best_mae > res_mae:
+      best_mae = res_mae
 
   print('Training finished...')
-  print('In the total training process, the best accuracy is ',best_acc,' the best mae is ',best_mae)
+  print('In the total training process, the best cs is ',best_kl,' the best mae is ',best_mae)
 
 if __name__ == '__main__':
     main()
